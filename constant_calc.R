9a844627b8239a17c1cd1a651e9721d5d7a1b090
liuaber <51933092+liuaber@users.noreply.github.com>
2023-10-02 07:36:20 -0500

    Update constant_calc.R

--- constant_calc.R
+++ constant_calc.R
@@ -5,3 +5,12 @@ f_gamma = function(alpha){
   gammainc(alpha+1,alpha)/gamma(alpha)+0.5*alpha^2-alpha }
 uniroot(f_gamma,interval = c(0.001,10))
 
+f_k = function(k){
+  alpha = 0.308289
+  k^alpha*exp(-k)/gamma(alpha)-alpha+k 
+}
+uniroot(f_k,interval = c(0.001,10))
+library(invgamma)
+1/(gammainc(0.3082908,0.144351)/gamma(alpha)+1)
+
+   
